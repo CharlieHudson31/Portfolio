ad3cbd8fbc3484aa9c04058e58d5eadc5bb12bd6
unknown <charlieohudson@gmail.com>
2025-07-05 18:13:17 -0700

    update port num

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 from flask import Flask
 app = Flask(__name__)
+import os
+
+
+port = int(os.environ.get("PORT", 5000))
 
 @app.route("/")
 def home():
     return "Hello from Railway!"
 
+
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0", port=port)
